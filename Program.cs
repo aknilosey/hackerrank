247730edee80a325c1408f2456aaf64f10603c39
Nilosey <nilosea@diebold.com>
2017-03-17 23:51:45 +0530

    adding code for reversing an array

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int i = 0;
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        int[] arrrev = new int[n];
+        int j = 0;
+        for (i = n - 1; i >= 0; i--)
+        {
+            arrrev[j] = arr[i];
+            Console.Write(arrrev[j].ToString() + " ");
+            j++;
+
+        }
+
+
+    }
+}
