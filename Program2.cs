f29cb3a68433fd3101b82d8a0d8a7f9e3868dd99
Nilosey <nilosea@diebold.com>
2017-03-18 00:47:18 +0530

    adding code for hacker rank dynamic array problem

---
+++ Program2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+        string input = Console.ReadLine();
+        //int input=Convert.ToInt32(input_temp);
+        int N = Convert.ToInt32(input.Split(' ')[0]);
+        int Q = Convert.ToInt32(input.Split(' ')[1]);
+        List<Int32>[] SeqList = new List<Int32>[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            SeqList[i] = new List<Int32>();
+        }
+
+        int lastAns = 0;
+        for (int i = 0; i < Q; i++)
+        {
+            string query = Console.ReadLine();
+            int num = Convert.ToInt32(query.Split(' ')[0]);
+
+            int x = Convert.ToInt32(query.Split(' ')[1]);
+            int y = Convert.ToInt32(query.Split(' ')[2]);
+
+            int index = (x ^ lastAns) % N;
+            if (num == 1)
+            {
+                SeqList[index].Add(y);
+
+
+            }
+            else if (num == 2)
+            {
+                lastAns = SeqList[index][y % SeqList[index].Count];
+                Console.WriteLine(lastAns);
+            }
+        }
+    }
+}
+
+
